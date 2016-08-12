b60273bd7713ccef230e0aa9b2e9f981b01d89dd
Cesar Landeros <cesar.landeros@corb.mx>
2016-08-12 18:37:36 -0500

    testing input from mattermost

--- main.js
+++ main.js
@@ -4,8 +4,19 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-type': 'application/json',
     });
-    console.log(req.body);
-    res.end('{"text": "dilci"}');
+    if (req.method == 'POST') {
+        var jsonString = '';
+
+        req.on('data', function (data) {
+            jsonString += data;
+        });
+
+        req.on('end', function () {
+            console.log(JSON.parse(jsonString));
+            res.end('{"text": "dilci dilci"}');
+        });
+    }
+
 }).listen(8080, '127.0.0.1');
 
 console.log('server running...');
