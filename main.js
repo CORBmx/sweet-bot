7a7e56ec4e0426c9ac48cb2ad34a2e5ff0630386
Cesar Landeros <cesar.landeros@corb.mx>
2016-08-12 18:33:03 -0500

    testing mattermost response

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ http.createServer(function (req, res) {
     res.writeHead(200, {
         'Content-type': 'application/json',
     });
-    console.log('que pedo');
-    res.end('{"text": "khe http://i.imgur.com/LjEuB5q.jpg"}');
+    console.log(req.body);
+    res.end('{"text": "dilci"}');
 }).listen(8080, '127.0.0.1');
 
 console.log('server running...');
