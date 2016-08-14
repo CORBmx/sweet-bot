b13192e7808776660577cc04bf6d2d808e53fe7f
Cesar Landeros <chichozell@gmail.com>
2016-08-13 22:39:21 -0500

    added body parser so the  app can get json data

--- app/router.js
+++ app/router.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 app.post('/', (req, res) => {
+    console.log(`text: ${req.body.text}`);
     const dulcefiedText = req.body.text.replace('dulce ', '').toLowerCase().replace(/[aeiouaáéíóú]/g, 'i')
 
     res.json( {
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./app/router');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/ladulce', router);
 
